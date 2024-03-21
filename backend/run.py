816906ad05b56d27c64273df5bdd3f719cb54b58
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-03-21 12:11:59 +0900

    Refactor: 파이어베이스 저장 삭제 및 함수 구조 변경

--- backend/run.py
+++ backend/run.py
@@ -1,40 +1,57 @@
 from flask import Flask
-# from flask_socketio import SocketIO
-import firebase_admin
-from firebase_admin import credentials
 from controller import api_bp
 import os
 from requests import get
 import tensorflow as tf
 
+
 def create_app():
+    app = Flask(__name__)
+    load_env(app)
+    setting()
+    load_model()
+    register_blueprint(app)
+
+    return app
+
+def load_env(app):
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # 경고와 오류 메시지만 표시
     tf.get_logger().setLevel('ERROR')  # 경고 무시
-    #my project id
-    PROJECT_ID = "carscratch"
+    app.config['SECRET_KEY'] = 'higaluctc4841'
 
-
+def load_model():
     url  = 'https://firebasestorage.googleapis.com/v0/b/carscratch.appspot.com/o/Model%2Fbeefprince_model.h5?alt=media&token=23767bf7-35ab-4a55-8bec-f6564bcb097a'
-    path = 'resource/car_scratch_model.h5'
+    path = 'resource/model/car_scratch_model.h5'
 
-    # 딥러닝 파일 저장하기, 최초 한번만 실행
+     # 딥러닝 파일 저장하기, 최초 한번만 실행
     if not os.path.isfile(path) :
         with open(path, 'wb') as f :
             ai_file = get(url)
             f.write(ai_file.content)
 
-    # # 파이어베이스 고유식별파일 => 이게 있어야 파이어베이스 접근허가
-    # cred = credentials.Certificate(r'.\res\service-account-file.json')
-    # # 초기값 설정
-    # default_app = firebase_admin.initialize_app(cred,{'storageBucket':f"{PROJECT_ID}.appspot.com"})
+def setting():
+    RESOURCE_DIR = 'resource'
+    MODEL_DIR = 'model'
+    IMG_DIR = 'image'
+    BASIC_IMG_DIR = 'basic'
+    MASK_IMG_DIR = 'mask'
 
-    # 객체생성 후 설정을 해주고 소켓과 연동해준다
-    app                      = Flask(__name__) # 객체생성 & 현재 실행중인 모듈 전달
+    # 기본 이미지와 마스크 이미지의 경로를 설정
+    basic_image_path = os.path.join(RESOURCE_DIR, IMG_DIR, BASIC_IMG_DIR)
+    mask_image_path = os.path.join(RESOURCE_DIR, IMG_DIR, MASK_IMG_DIR)
+    model_path = os.path.join(RESOURCE_DIR, MODEL_DIR)
+
+    from util.scretch_model import Model
+    Model()
+
+    # 디렉토리가 없으면 생성합니다. exist_ok=True는 디렉토리가 이미 존재하면 무시
+    os.makedirs(basic_image_path, exist_ok=True)
+    os.makedirs(mask_image_path, exist_ok=True)
+    os.makedirs(model_path, exist_ok=True)
+
+
+def register_blueprint(app):
     app.register_blueprint(api_bp)
-    app.config['SECRET_KEY'] = 'higaluctc4841'
-    # soketio                  = SocketIO(app)
-
-    return app
 
 if __name__ == '__main__':
     soketio = create_app()
